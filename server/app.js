fe2d2c2f02eb9fe887925de141b3edb78162b516
Maxim Trekusov <trekusovmax@gmail.com>
2022-03-28 22:08:23 +0300

    added routes

--- server/app.js
+++ server/app.js
@@ -1,14 +1,17 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const config = require("config")
-const chalk = require("chalk")
-const initDataBase = require("./startUp/initDataBase")
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const chalk = require('chalk')
+const initDataBase = require('./startUp/initDataBase')
+const routes = require('./routes')
 
 const app = express()
-const PORT = config.get("port") ?? 8080
+const PORT = config.get('port') ?? 8080
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use('/api', routes)
 
 /* if (process.env.NODE_ENV === "production") {
 	console.log("Production")
@@ -17,20 +20,20 @@ app.use(express.urlencoded({ extended: false }))
 } */
 
 async function start() {
-	try {
-		mongoose.connection.once("open", () => {
-			initDataBase()
-		})
+  try {
+    mongoose.connection.once('open', () => {
+      initDataBase()
+    })
 
-		await mongoose.connect(config.get("mongoUri"))
-		console.log(chalk.green(`MongoDB connected`))
-		app.listen(PORT, () => {
-			console.log(chalk.green(`Server has been started on port ${PORT}`))
-		})
-	} catch (e) {
-		console.log(chalk.red(e.message))
-		process.exit(1)
-	}
+    await mongoose.connect(config.get('mongoUri'))
+    console.log(chalk.green(`MongoDB connected`))
+    app.listen(PORT, () => {
+      console.log(chalk.green(`Server has been started on port ${PORT}`))
+    })
+  } catch (e) {
+    console.log(chalk.red(e.message))
+    process.exit(1)
+  }
 }
 
 start()
