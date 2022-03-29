40cf864e4cd3430dc613e64fb1c66c23f8cf1ee3
unknown <admin@DTO-112-new5.interrao.ru>
2022-03-29 16:42:40 +0300

    add logic for register

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,39 @@
-const express = require('express')
+const express = require("express")
+const User = require("../models/User")
+const bcrypt = require("bcryptjs")
 const router = express.Router({ mergeParams: true })
+const generateUserData = require("../utils/helpers")
+const tokenService = require("../services/token.service")
 
-router.post('/signUp', async (req, res) => {})
-router.post('/signInWithPassword', async (req, res) => {})
-router.post('/token', async (req, res) => {})
+router.post("/signUp", async (req, res) => {
+	try {
+		const { email, password } = req.body
+		const existingUser = await User.findOne({ email })
+		if (existingUser) {
+			return res.status(400).json({
+				error: {
+					message: "EMAIL_EXISTS",
+					code: 400
+				}
+			})
+		}
+		const hashedPassword = await bcrypt.hash(password, 12)
+
+		const newUser = User.create({
+			...generateUserData(),
+			...req.body,
+			password: hashedPassword
+		})
+
+		const tokens = tokenService.generate({ _id: newUser._id })
+		res.status(201).send({ ...tokens, userId: newUser._id })
+	} catch (error) {
+		res.status(500).json({
+			message: "На сервере произошла ошибка. Попробуйте позже"
+		})
+	}
+})
+router.post("/signInWithPassword", async (req, res) => {})
+router.post("/token", async (req, res) => {})
 
 module.exports = router
---
+++ server/services/token.service.js
@@ -0,0 +1,5 @@
+class TokenServise {
+	generate() {}
+}
+
+module.exports = new TokenServise()
