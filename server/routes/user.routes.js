5897921d648c5c40cd5b3db75bf6be0fee0e4e84
unknown <admin@DTO-112-new5.interrao.ru>
2022-03-31 11:54:30 +0300

    added comment

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const User = require("../models/User")
 const router = express.Router({ mergeParams: true })
+const auth = require("../middleware/auth.middleware")
 
-router.patch("/:userId", async (req, res) => {
+router.patch("/:userId", auth, async (req, res) => {
 	try {
-		const userId = req.params
+		const { userId } = req.params
 		if (userId) {
 			const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true })
 			res.send(updatedUser)
@@ -12,13 +13,14 @@ router.patch("/:userId", async (req, res) => {
 			return res.status(401).json({ message: "Unauthorized" })
 		}
 	} catch (error) {
+		console.log(error)
 		res.status(500).json({
 			message: "На сервере произошла ошибка. Попробуйте позже"
 		})
 	}
 })
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
 	try {
 		const list = await User.find()
 		res.send(list)
