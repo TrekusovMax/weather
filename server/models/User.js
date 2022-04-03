5df13397cde76b71f5481f0b99606ffc8cb7753d
Maxim Trekusov <trekusovmax@gmail.com>
2022-04-03 20:56:18 +0300

    modified User

--- server/models/User.js
+++ server/models/User.js
@@ -5,12 +5,7 @@ const schema = Schema(
 		name: { type: String },
 		email: { type: String, required: true, unique: true },
 		password: { type: String },
-		completedMeetings: Number,
-		image: String,
-		profession: { type: Schema.Types.ObjectId, ref: "Profession" },
-		qualities: { type: Schema.Types.ObjectId, ref: "Qualitiy" },
-		rate: Number,
-		sex: { type: String, enum: ["male", "female", "other"] }
+		favorites: { type: Schema.Types.ObjectId, ref: "Favorites" }
 	},
 	{
 		timestamps: true
