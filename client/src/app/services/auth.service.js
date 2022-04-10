56908f64c3798b272816a7ab9eca9d1c00c90f67
Maxim Trekusov <trekusovmax@gmail.com>
2022-04-10 22:34:56 +0300

    config client

--- client/src/app/services/auth.service.js
+++ client/src/app/services/auth.service.js
@@ -1,29 +1,31 @@
-import axios from 'axios'
+import axios from "axios"
+import localStorageService from "./localStorage.service"
+import config from "../config.json"
 
 const httpAuth = axios.create({
-  baseURL: 'https://identitytoolkit.googleapis.com/v1/',
-  params: {
-    key: process.env.REACT_APP_FIREBASE_KEY,
-  },
+	baseURL: config.apiEndpoint + "/auth/",
+	params: {
+		key: process.env.REACT_APP_FIREBASE_KEY
+	}
 })
 
 const authService = {
-  register: async ({ email, password }) => {
-    const { data } = await httpAuth.post(`accounts:signUp`, {
-      email,
-      password,
-      returnSecureToken: true,
-    })
-    return data
-  },
-  login: async ({ email, password }) => {
-    const { data } = await httpAuth.post(`accounts:signInWithPassword`, {
-      email,
-      password,
-      returnSecureToken: true,
-    })
-    return data
-  },
+	register: async ({ email, password }) => {
+		const { data } = await httpAuth.post(`signUp`, {
+			email,
+			password,
+			returnSecureToken: true
+		})
+		return data
+	},
+	login: async ({ email, password }) => {
+		const { data } = await httpAuth.post(`signInWithPassword`, {
+			email,
+			password,
+			returnSecureToken: true
+		})
+		return data
+	}
 }
 
 export default authService
