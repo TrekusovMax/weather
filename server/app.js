4e4a3b1434025e779c68f8ea4960d1e00e44e249
Maxim <maxtrekusovsite@gmail.com>
2023-01-12 23:25:42 +0300

    deploy

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const config = require("config")
-const chalk = require("chalk")
-const path = require("path")
-const cors = require("cors")
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const chalk = require('chalk')
+const path = require('path')
+const cors = require('cors')
 
-const routes = require("./routes")
+const routes = require('./routes')
 
 const app = express()
 
@@ -13,32 +14,29 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
-app.use("/api", routes)
+app.use('/api', routes)
 
-const PORT = config.get("port") ?? 8080
+const PORT = config.get('port') ?? 8080
 
-// if (process.env.NODE_ENV === 'production') {
-//   console.log('Production')
-// } else {
-//   console.log('Development')
-// }
-if (process.env.NODE_ENV === "production") {
-	app.use("/", express.static(path.join(__dirname, "client")))
-	const indexPath = path.join(__dirname, "client", "index.html")
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client')))
+  const indexPath = path.join(__dirname, 'client', 'index.html')
 
-	app.get("*", (req, res) => {
-		res.sendFile(indexPath)
-	})
+  app.get('*', (req, res) => {
+    res.sendFile(indexPath)
+  })
 }
 async function start() {
-	try {
-		await mongoose.connect(config.get("mongoUri"))
-		console.log(chalk.green(`MongoDB connected.`))
-		app.listen(PORT, () => console.log(chalk.green(`Server has been started on port ${PORT}...`)))
-	} catch (e) {
-		console.log(chalk.red(e.message))
-		process.exit(1)
-	}
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log(chalk.green(`MongoDB connected.`))
+    app.listen(PORT, () =>
+      console.log(chalk.green(`Server has been started on port ${PORT}...`)),
+    )
+  } catch (e) {
+    console.log(chalk.red(e.message))
+    process.exit(1)
+  }
 }
 
 start()
